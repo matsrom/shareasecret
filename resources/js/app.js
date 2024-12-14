6dfb911dfb829a6e1774caf5b80a4794f822fe96
Mats <mats@acerta-cert.es>
2024-12-14 19:03:33 +0100

    asd

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,12 @@ import "./cryptogtaphy";
 import "flowbite";
 import Toastify from "toastify-js";
 import L from "leaflet";
+
 window.Toastify = Toastify;
+
 window.L = L;
-// import Alpine from "alpinejs";
-
-// window.Alpine = Alpine;
-
-// Alpine.start();
+if (typeof L !== "undefined") {
+    console.log("Leaflet se ha importado correctamente.");
+} else {
+    console.error("Error: Leaflet no se ha importado.");
+}
