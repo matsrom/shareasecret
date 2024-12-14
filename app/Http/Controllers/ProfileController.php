bc8042e91e1a4f7c80f1976ac55bd787c8707e48
Mats <mats@acerta-cert.es>
2024-12-14 20:08:27 +0100

    Profile update toast

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -35,7 +35,10 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit')->with('status', [
+            'message' => 'Email updated successfully',
+            'class' => 'toast-success',
+        ]);
     }
 
     /**
